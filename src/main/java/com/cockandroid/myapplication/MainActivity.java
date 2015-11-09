8371068050028c38d782f9d4edfd33f047776443
Seko213 <splatt213@naver.com>
2015-11-09 21:48:31 +0900

    Initial commit

---
+++ src/main/java/com/cockandroid/myapplication/MainActivity.java
@@ -0,0 +1,62 @@
+package com.cockandroid.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+        web.setWebViewClient(new SeokWeb());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = "http://";
+                if (edtUrl.getText().toString().contains(str)) {
+                    web.loadUrl(edtUrl.getText().toString());
+                }
+                else  {
+                    web.loadUrl(str + edtUrl.getText().toString());
+                }
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                web.goBack();
+            }
+        });
+    }
+
+    class SeokWeb extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            edtUrl.setText(url);
+        }
+    }
+}
